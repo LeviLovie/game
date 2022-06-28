5889178052a3bb83d9693ae3fa8cad20e95c2139
Lev111111 <leviiloviee@gmail.com>
2022-06-28 23:18:22 +0700

    test

--- Game.java
+++ Game.java
@@ -1,8 +1,39 @@
-import java.awt.*;
-import javax.swing.JFrame;
-
 public class Game extends Canvas implements Runnable {
+    private static final long serialVertionUID = 1l;
+
+    public void start() {
+        boolean running = true;
+        new Thread(this).start();
+    }
+
+    @Override
+    public void run() {
+        long lastTime = System.currentTimeMillis();
+        long delta;
+
+        init();
+
+        boolean running;
+        while (running) {
+            delta = System.currentTimeMillis() - lastTime;
+            lastTime = System.currentTimeMillis();
+            update();
+            render();
+        }
+    }
+
+    public void init() {
+
+    }
+
+    public void render() {
+        
+    }
+
+    public void update() {
+        
+    }
+    
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
     }
 }
