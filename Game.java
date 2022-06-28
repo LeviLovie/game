b138859715ea6a166feba9fb02c11bd58b63b941
Lev111111 <leviiloviee@gmail.com>
2022-06-28 23:08:17 +0700

    Create Game.java

---
+++ Game.java
@@ -0,0 +1,8 @@
+import java.awt.*;
+import javax.swing.JFrame;
+
+public class Game extends Canvas implements Runnable {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
