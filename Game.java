6489bdf148d41a2d86bec105c4c5ed3c4f6b22ff
Lev111111 <leviiloviee@gmail.com>
2022-06-29 10:34:38 +0700

    no

--- Game.java
+++ Game.java
@@ -1,36 +1,37 @@
-import java.awt.*;
-import javax.swing.*;
+import java.awt.Color;
+import java.awt.Graphics;
+// import java.awt.Point;
+import javax.swing.JPanel;
 
-class Screen {
-    void start() {
-        // creating object of JFrame(Window popup)
-        JFrame window = new JFrame();
-  
-        // setting closing operation
-        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-  
-        // setting size of the pop window
-        window.setBounds(30, 30, 200, 200);
-  
-        // setting canvas for draw
-        window.getContentPane().add(paint(g));
-  
-        // set visibility
-        window.setVisible(true);
+import game_util.*;
+
+/**
+ * A panel maintaining a picture of a Do Not Enter sign.
+ */
+ public class Game extends JPanel {
+    /**
+     * Called by the runtime system whenever the panel needs painting.
+     */
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.setColor(Color.BLACK);
+        // g.fillOval(0, 0, 100, 100);  
+        // g.setColor(Color.RED);
+        // g.fillOval(center.x - innerRadius, center.y - innerRadius, innerDiameter, innerDiameter);
+        // g.setColor(Color.WHITE);
+        g.fillRect(0, 0, 200, 100);
     }
 
-    void paint(Graphics g)
-    {
-  
-        // draw and display the line
-        g.drawLine(30, 20, 80, 90);
+    public void painting(Graphics g, short x, short y, short width, short height) {
+        super.paintComponent(g);
+        g.setColor(Color.BLACK);
+        g.fillRect(x, y, width, height);
     }
-}
 
-
-public class Game {
+    /**
+     * A little driver in case you want a stand-alone application.
+     */
     public static void main(String[] args) {
-        Screen screen = new Screen();
-        screen.start();
+        GameUtilitys gametilitys;
     }
 }
